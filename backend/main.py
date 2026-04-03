7f9a653642333822325efc2d4296c51f79f7a943
sharadvyas123 <sharadvyas132@gmail.com>
2026-04-03 23:58:28 +0530

    minor changes

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from backend!")
+# from elevenlabs.client import ElevenLabs
+# from dotenv import load_dotenv
+# load_dotenv()
+# import os
+# client = ElevenLabs(
+#     api_key=os.getenv("ELEVEN_LABS_API_KEY")
+# )
 
+# audio = client.text_to_speech.convert(
+#     text="Reality patched. Nginx is back online.",
+#     voice_id="21m00Tcm4TlvDq8ikWAM",  # default voice
+#     model_id="eleven_multilingual_v2"
+# )
 
-if __name__ == "__main__":
-    main()
+# with open("output.mp3", "wb") as f:
+#     for chunk in audio:
+#         f.write(chunk)
