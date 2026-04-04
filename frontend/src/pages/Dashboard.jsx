1f348d0ea92e0975e09dafe0785890eab2ac03c9
divyansh2901 <divyanshkashyap2901@gmail.com>
2026-04-04 14:23:25 +0530

    fixed

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -18,10 +18,10 @@ export default function Dashboard() {
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-    // if (!token) {
-    //   navigate('/');
-    //   return;
-    // }
+    if (!token) {
+      navigate('/');
+      return;
+    }
 
     try {
       const base64Url = token.split('.')[1];
